36c89970d162fd5ee849c48fcffef3e7019bc64f
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2024-03-29 04:24:43 +0500

    structure chnaged

--- app/template.jsx
+++ app/template.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { motion } from 'framer-motion'
-import useScrollProgress from "@/hooks/useScrollProgress"
-
+import useScrollProgress from './hooks/useScrollProgress'
 const variants = {
     hidden: { opacity: 0 },
     enter: { opacity: 1 }
