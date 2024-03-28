37e365643bf03790d694869a2ba885bc7e8442cf
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2024-03-29 04:17:21 +0500

    clean

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,4 @@
-'use client'
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,19 +2,22 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-const links =[
-  {path:'/', name:'home'},
-  {path:'/projects', name:'my projects'},
-  {path:'/contact', name:'Contact'},
+const links = [
+  { path: '/', name: 'home' },
+  { path: '/projects', name: 'my projects' },
+  { path: '/contact', name: 'Contact' },
 ]
-const Nav = ({containerStyles, linkStyles, underlineStyles}) => {
-    const path=usePathname();
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+  const path = usePathname();
 
   return (
     <nav className={`${containerStyles}`}>
-      {links.map((link,index)=>{
-        return(
-          <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>{link.name}</Link>
+      {links.map((link, index) => {
+        return (
+          <Link href={link.path} key={index} className={`capitalize ${linkStyles}`}>
+            {link.path==path}
+            {link.name}
+          </Link>
         )
       })}
     </nav>
