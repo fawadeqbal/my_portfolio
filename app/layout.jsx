ac948d6578e981028c86d588803d2c9bee7f0bc2
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2024-03-29 04:39:58 +0500

    updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ThemeProvider } from "@/components/ThemeProvider";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { ThemeProvider } from "../components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
