7defc467a719e19e93e3a633729c207868fd4f3d
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2024-04-15 23:13:05 +0500

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -20,7 +20,6 @@ import Socials from './Socials'
 const Hero = () => {
 
     const handleDownload = () => {
-       
         const link = document.createElement('a');
         link.href = '/fawad_iqbal_CV.pdf'; // Specify the path to your CV file
         link.download = 'fawad_iqbal_CV.pdf'; // Specify the filename for the downloaded file
@@ -29,7 +28,7 @@ const Hero = () => {
         document.body.removeChild(link);
     };
     return (
-        <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
+        <section className='py-5 xl:py-15 h-[84vh] xl:pt-15 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
             <div className="container mx-auto">
                 <div className="flex justify-between gap-x-8">
                     {/* {text} */}
@@ -40,26 +39,26 @@ const Hero = () => {
 
                         <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
                             <Link href={'/contact'}>
-                                <Button className='gap-x-2'>Contact me <Send size={18}/></Button>
+                                <Button className='gap-x-2'>Contact me <Send size={18} /></Button>
                             </Link>
-                          
-                                <Button onClick={handleDownload} variant='secondary' className='gap-x-2'>Download CV <Download path='mycv.png' size={18}/></Button>
-                           
+
+                            <Button onClick={handleDownload} variant='secondary' className='gap-x-2'>Download CV <Download path='mycv.png' size={18} /></Button>
+
                         </div>
 
-                        <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0' iconsStyles='text-foreground text-[22px] hover:text-primary transition-all'/>
+                        <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0' iconsStyles='text-foreground text-[22px] hover:text-primary transition-all' />
                     </div>
                     {/* {img} */}
                     <div className="hidden xl:flex relative">
                         {/* <Badge containerStyles="absolute top-[24%] -left-[5rem]" icon={<RiBriefcase4Fill/>} endCountNum={3} badgeText="Years of experience" /> */}
                         <div className='bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[580px] bg-no-repeat absolute -top-1 -right-2'>
-                            <DevImg containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom' imgSrc='/about/developer.png'/>
+                            <DevImg containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom' imgSrc='/about/developer.png' />
                         </div>
                     </div>
                 </div>
                 {/* {icon} */}
                 <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
-                    <RiArrowDownSLine className='text-3xl text-primary'/>
+                    <RiArrowDownSLine className='text-3xl text-primary' />
                 </div>
             </div>
         </section>
